d3b033a3544664573b2011542395b232915ef389
suhan-style <suhan@gmail.com>
2024-04-09 18:11:42 +0530

    Updates

---
+++ precache-manifest.963fe516a3346b35aeea81e7d63abd98.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b316327b29e179efc4cd",
+    "url": "/github.io/reactmoonex.git/static/css/main.cb25fee7.chunk.css"
+  },
+  {
+    "revision": "b316327b29e179efc4cd",
+    "url": "/github.io/reactmoonex.git/static/js/main.b316327b.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/github.io/reactmoonex.git/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "7d05305ba43998912190",
+    "url": "/github.io/reactmoonex.git/static/css/2.968a6a00.chunk.css"
+  },
+  {
+    "revision": "7d05305ba43998912190",
+    "url": "/github.io/reactmoonex.git/static/js/2.7d05305b.chunk.js"
+  },
+  {
+    "revision": "7ad45ce0db1efd66ad37",
+    "url": "/github.io/reactmoonex.git/static/js/runtime~main.7ad45ce0.js"
+  },
+  {
+    "revision": "f4cb1154c306bae3235d2a4a4b037f53",
+    "url": "/github.io/reactmoonex.git/static/media/Cerebri Sans Book.f4cb1154.ttf"
+  },
+  {
+    "revision": "017b088bcfadd28e733e98bce5a8f64a",
+    "url": "/github.io/reactmoonex.git/static/media/testimonial-bg.017b088b.png"
+  },
+  {
+    "revision": "4180e23f787abc8ec458f9224ffebea1",
+    "url": "/github.io/reactmoonex.git/static/media/Cerebri Sans Bold.4180e23f.ttf"
+  },
+  {
+    "revision": "a743b2a8e5044d418d5d85ec9a47f49d",
+    "url": "/github.io/reactmoonex.git/static/media/main-image.a743b2a8.png"
+  },
+  {
+    "revision": "4e5720eb83dd0ec371b8e75619264c9e",
+    "url": "/github.io/reactmoonex.git/static/media/Cerebri Sans Heavy.4e5720eb.ttf"
+  },
+  {
+    "revision": "562a9e390c4c081cc810b025ce0dc919",
+    "url": "/github.io/reactmoonex.git/static/media/Cerebri Sans ExtraBold.562a9e39.ttf"
+  },
+  {
+    "revision": "86d929bd6bb81888e9be3f39fe14b96a",
+    "url": "/github.io/reactmoonex.git/index.html"
+  }
+];
